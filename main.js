dfcc6257dbf2f41498a3316d6ae8a8ca467551e6
Lucas Reis <49770374+LucasReis-DEV0@users.noreply.github.com>
2023-03-23 20:40:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
-function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
 const ListaDeTeclas = document.querySelectorAll('.tecla');
 
-ListaDeTeclas[0].onclick = tocaSomPom;
+
+
+//para
+for ( let contador =0; contador < ListaDeTeclas.length; contador++) {
+
+        const tecla = ListaDeTeclas[contador];
+        const instrumento = tecla.classList[1];
+
+        //console.log(instrumento)
+
+        //template string
+        const idAudio = `#som_${instrumento}`;
+        //console.log(idAudio);
+
+        tecla.onclick = function(){
+            tocaSom(idAudio)
+        }
+
+        tecla.onkeydown = function(evento){
+
+            console.log(evento.code == 'Space')
+
+            if (evento.code === 'Space' || evento.code === 'Enter'){ 
+                tecla.classList.add('ativa');
+            }
+ 
+}
+        
+        tecla.onkeyup = function(){
+            tecla.classList.remove('ativa');
+    }
+
+        console.log(contador);
+
+}
+
+//
