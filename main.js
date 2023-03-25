1560360537011fd8dd7ca6bcd58c7e05b0b52f26
Lucas Reis <49770374+LucasReis-DEV0@users.noreply.github.com>
2023-03-25 12:39:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-function tocaSom (idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento === null){
+        //alert('Elemento não encontrado')
+        
+    }
+
+    if(elemento != null && elemento.localName === 'audio'){
+            elemento.play();
+    }
+    else{
+        alert('Elemento não encontrado')
+        console.log('elemento não encontrado')
+    }
+
 }
 
 const ListaDeTeclas = document.querySelectorAll('.tecla');
